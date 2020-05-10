0258964cdba4565e1da01e0b6e958f94f47dfef6
Jonathan Geffen <t-jogeff@microsoft.com>
2020-05-10 16:26:17 +0200

    support db caching in word encoder

--- WordEnc.py
+++ WordEnc.py
@@ -1,11 +1,11 @@
 from torchnlp.word_to_vector import GloVe
-
+# pip install pytorch-nlp
 
 
 
 class WordEncoding_auto:
-    def __init__(self, dataset_type, vectorDim):
-        self.vectors = GloVe(name=dataset_type, dim=vectorDim)
+    def __init__(self, dataset_type='6B', vector_dim=100):
+        self.vectors = GloVe(name=dataset_type, dim=vector_dim, cache='.\\glove')
     
     def get_word_Vector(self, word):
         return self.vectors[word]
