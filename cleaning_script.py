1e560145db4b9b9b84fa026bf6ad1e059c56e218
federicojv <fjv@kth.se>
2020-05-12 16:30:13 +0200

    name mismatch

--- cleaning_script.py
+++ cleaning_script.py
@@ -3,53 +3,53 @@ import re
 import matplotlib.pyplot as plt
 import langdetect
 
-file = pd.read_csv('lyrics.csv')
-print('Initial number of songs: {}'.format(file.shape[0]))
+pd = pd.read_csv('lyrics.csv')
+print('Initial number of songs: {}'.format(pd.shape[0]))
 
 
 ## REMOVE YEAR COLUMN ##
-file = file.drop('year', 1)
+pd = pd.drop('year', 1)
 
 
 ## REMOVE SONGS WITH NO LYRICS ##
 print('Removing songs with no lyrics')
-file = file.dropna()
-print('Current number of songs: {}'.format(file.shape[0]))
+pd = pd.dropna()
+print('Current number of songs: {}'.format(pd.shape[0]))
 
 
 ## REMOVE SONG WITH GENRE LISTED AS 'OTHER' OR 'NOT AVAILABLE' ##
-file = file[file.genre != 'Other']
-file = file[file.genre != 'Not Available']
+pd = pd[pd.genre != 'Other']
+pd = pd[pd.genre != 'Not Available']
 
 
 ## REMOVE SONG WITH ONLY ONE VERSE ##
 print('Removing songs with only one verse')
-file = file[file.lyrics.str.contains('\n')]
-print('Current number of songs: {}'.format(file.shape[0]))
+pd = pd[pd.lyrics.str.contains('\n')]
+print('Current number of songs: {}'.format(pd.shape[0]))
 
 
 ## REMOVE SONGS WITH TOO MANY OR NOT ENOUGH VERSES ##
 print('Removing songs with too many or too few verses')
-file['verse_count'] = file.lyrics.str.count('\n') + 1
-avg_verses = file['verse_count'].mean()
-file = file[file.lyrics.str.count('\n') > avg_verses * 0.05]
-file = file[file.lyrics.str.count('\n') < avg_verses * 1.95]
-print('Current number of songs: {}'.format(file.shape[0]))
+pd['verse_count'] = pd.lyrics.str.count('\n') + 1
+avg_verses = pd['verse_count'].mean()
+pd = pd[pd.lyrics.str.count('\n') > avg_verses * 0.05]
+pd = pd[pd.lyrics.str.count('\n') < avg_verses * 1.95]
+print('Current number of songs: {}'.format(pd.shape[0]))
 
 
 ## REMOVE SHORT AND LONG LYRICS ##
 print('Removing songs with very short or very longs lyrics')
-file['char_count'] = file['lyrics'].apply(len)
-avg_chars = file['char_count'].mean()
-file = file[file.char_count > avg_chars * 0.05]
-file = file[file.char_count < avg_chars * 1.95]
-print('Current number of songs: {}'.format(file.shape[0]))
+pd['char_count'] = pd['lyrics'].apply(len)
+avg_chars = pd['char_count'].mean()
+pd = pd[pd.char_count > avg_chars * 0.05]
+pd = pd[pd.char_count < avg_chars * 1.95]
+print('Current number of songs: {}'.format(pd.shape[0]))
 
 
 ## REMOVE LYRICS WITH NO SPACES INBETWEEN WORDS ##
 print('Removing songs with no spaces inbetween words')
-file = file[file.lyrics.str.count(' ') != 0]
-print('Current number of songs: {}'.format(file.shape[0]))
+pd = pd[pd.lyrics.str.count(' ') != 0]
+print('Current number of songs: {}'.format(pd.shape[0]))
 
 
 ## REMOVE SONGS NOT IN ENGLISH ##
@@ -67,17 +67,17 @@ def lang_detector(song):
         return False
 
 
-file['isEnglish'] = file['lyrics'].apply(lang_detector)
-file = file[file['isEnglish'] == True]
-print('Final number of songs: {}'.format(file.shape[0]))
+pd['isEnglish'] = pd['lyrics'].apply(lang_detector)
+pd = pd[pd['isEnglish'] == True]
+print('Final number of songs: {}'.format(pd.shape[0]))
 
 
 ## DROP OUTDATED COLUMNS ##
-file = file.drop('verse_count', 1)
-file = file.drop('char_count', 1)
-file = file.drop('isEnglish', 1)
+pd = pd.drop('verse_count', 1)
+pd = pd.drop('char_count', 1)
+pd = pd.drop('isEnglish', 1)
 
 
 ## SAVE FILE TO CSV ##
-file.to_csv('dataset.csv', index=False)
+pd.to_csv('dataset.csv', index=False)
 print('########### CLEANING COMPLETE ###########')
