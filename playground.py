78b3818047c66ecaa9cef04b8fd4e081ba069ba6
Jonathan Geffen <t-jogeff@microsoft.com>
2020-05-16 11:06:20 +0200

    initial HAN implementation

--- playground.py
+++ playground.py
@@ -1,19 +1,28 @@
 import datetime
 
 import majority_classifier
-from dataset_loader import LyricsDataset, WordAverageTransform
+from dataset_loader import LyricsDataset, WordAverageTransform, LyricsDatasetHAN, LyricsDatasetEmbeddedHAN
+import dataset_loader
 from dataset_metadata import DatasetMetadata, JSON_FILE_PATH
+import hierarchical_attention_net
+from hierarchical_attention_net import HAN
 from learning_dataset_generator import LEARNING_DATASET_TRAIN_PATH, LEARNING_DATASET_TEST_PATH, \
-    LEARNING_SMALL_DATASET_TRAIN_PATH
+    LEARNING_SMALL_DATASET_TRAIN_PATH, LEARNING_TINY_DATASET_TRAIN_PATH, LEARNING_TINY_DATASET_TEST_PATH
 from majority_classifier import MajorityClassifier
 import logistic_regresssion
 import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+
+from torch.utils.data import DataLoader
 
 import torch
 
 from plot_utils import plot_confusion_matrix
 
+WORD_EMBEDDING_PATH = '.\\glove\\glove.6B.100d.txt'
 LR_MODEL_PATH = '.\\models\\logistic_regression.model'
+HAN_MODEL_PATH = '.\\models\\han.model'
 
 
 def write_dataset_metadata():
@@ -26,6 +35,52 @@ def load_dataset_from_file():
     print(dm.genre_labels)
     print(dm.song_count)
     print(dm.genre_labels[dm.most_common_genre_id])
+
+
+def train_HAN_and_save(model_path, train_dataset, test_dataset):
+    word_embedding = pd.read_csv(WORD_EMBEDDING_PATH, header=None, sep=" ", quoting=csv.QUOTE_NONE).values[:, 1:]
+    model = HAN(hierarchical_attention_net.HIDDEN_SIZE, hierarchical_attention_net.HIDDEN_SIZE,
+                hierarchical_attention_net.BATCH_SIZE, hierarchical_attention_net.NUM_CLASSES,
+                word_embedding)
+
+    accuracy = hierarchical_attention_net.test_network(model, test_dataset)
+    print(f'{datetime.datetime.now()} - model accuracy before training - {accuracy}')
+    model = hierarchical_attention_net.train_network(model, train_dataset, 2)
+    print(f'{datetime.datetime.now()} - model finished training')
+    torch.save(model.state_dict(), model_path)
+
+
+def load_HAN_and_test(model_path, test_dataset):
+    word_embedding = pd.read_csv(WORD_EMBEDDING_PATH, header=None, sep=" ", quoting=csv.QUOTE_NONE).values[:, 1:]
+    model = HAN(hierarchical_attention_net.HIDDEN_SIZE, hierarchical_attention_net.HIDDEN_SIZE,
+                hierarchical_attention_net.BATCH_SIZE, hierarchical_attention_net.NUM_CLASSES,
+                word_embedding)
+    model.load_state_dict(torch.load(model_path))
+    accuracy = hierarchical_attention_net.test_network(model, test_dataset)
+
+    dm = DatasetMetadata.from_filepath(JSON_FILE_PATH)
+    #plt.figure(figsize=(10, 10))
+    #plot_confusion_matrix(confusion, dm.genre_labels, title="HAN Confusion Matrix")
+    #plt.show()
+    return accuracy
+
+
+def test_HAN(tiny=True):
+    test_dataset_path = LEARNING_DATASET_TEST_PATH
+    train_dataset_path = LEARNING_SMALL_DATASET_TRAIN_PATH
+    if(tiny):
+        test_dataset_path = LEARNING_TINY_DATASET_TEST_PATH
+        train_dataset_path = LEARNING_TINY_DATASET_TRAIN_PATH
+
+    print(f'{datetime.datetime.now()} - starting HAN testing')
+    train_dataset = LyricsDatasetEmbeddedHAN(train_dataset_path, WORD_EMBEDDING_PATH)
+    print(f'{datetime.datetime.now()} - loaded training data')
+    test_dataset = LyricsDatasetEmbeddedHAN(test_dataset_path, WORD_EMBEDDING_PATH)
+    print(f'{datetime.datetime.now()} - loaded testing data')
+
+    train_HAN_and_save(HAN_MODEL_PATH, train_dataset, test_dataset)
+    accuracy = load_HAN_and_test(HAN_MODEL_PATH, test_dataset)
+    print(f'{datetime.datetime.now()} - model accuracy after training - {accuracy}')
 
 
 def test_majority_classifier():
@@ -103,4 +158,6 @@ def test_word_average():
 
 # test_logistic_regression()
 
-test_majority_classifier()
+# test_majority_classifier()
+
+test_HAN()
